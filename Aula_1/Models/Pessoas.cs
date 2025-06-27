79246c0b91e7c0bed508346bc2667cf386ad7e29
RaulCSiqueira <raulcesarsiqueira@gmail.com>
2025-06-27 15:12:19 -0300

    fim aula 1

--- Aula_1/Models/Pessoas.cs
+++ Aula_1/Models/Pessoas.cs
@@ -1,14 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Theading.Tasks;
+//using System.Theading;
+//using System.Theading.Tasks;
 
 namespace ExemploFundamentos.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; }
-        public int Idade { get; set; }
+        public string? Nome { get; set; }
+        public int? Idade { get; set; }
 
         public void Apresentar()
         {
--- Aula_1/Program.cs
+++ Aula_1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploFundamentos.Models;
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine($"Data atual: {dataAtual.ToString("MM/dd/yyyy HH:mm:ss")}");
+Console.WriteLine(dataAtual);
+
+dataAtual = DateTime.Now.AddDays(10);
+Console.WriteLine($"Data atual: {dataAtual.ToString("MM/dd/yyyy HH:mm:ss")}");
+Console.WriteLine(dataAtual);
+
+
+// string apresentacao = "Olá, seja bem vindo";
+// int quantidade = 1;
+// quantidade = 10;
+// double altura = 1.75211;
+// decimal preco = 1.75m;
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine($"Quantidade: {quantidade}");
+// Console.WriteLine($"Altura: {altura.ToString("F4")}");
+// Console.WriteLine($"Preço: {preco}");
+// Console.WriteLine($"Condição: {condicao}");
+
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Ra";
+// pessoa1.Idade = 30;
+// pessoa1.Apresentar();
